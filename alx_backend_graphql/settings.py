8d445cbaafa7901e6fe0057117e903bebacc62ef
Lesego916 <segobuyisile@gmail.com>
2026-01-23 19:16:39 +0200

    PBE

---
+++ alx_backend_graphql/settings.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "dummy-secret-key"
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    "graphene_django",
+    "django_filters",
+
+    "crm",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+ROOT_URLCONF = "alx_backend_graphql_crm.urls"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+STATIC_URL = "/static/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql_crm.schema.schema"
+}
--- settings.py
+++
@@ -1,9 +0,0 @@
-INSTALLED_APPS = [
-    ...
-    'django_filters',
-    'graphene_django',
-    'crm',
-]
-GRAPHENE = {
-    "SCHEMA": "alx_backend_graphql_crm.schema.schema"
-}
